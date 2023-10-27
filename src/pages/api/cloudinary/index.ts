4ffc115c0c8229d705363e6d6d5a20646e4b7d30
DarwinVA <darwin97.va@gmail.com>
2023-10-26 21:45:41 -0500

    save .env.example

--- src/pages/api/cloudinary/index.ts
+++ src/pages/api/cloudinary/index.ts
@@ -1,10 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { v2 as cloudinary } from "cloudinary";
+import { env } from "@/env.mjs";
 
 cloudinary.config({
   cloud_name: "dpcglvg6o", // add your cloud_name
-  api_key: "132176639719887", // add your api_key
-  api_secret: "I9f7-4MCAxSTxNPi33MJliNXEKE", // add your api_secret
+  api_key: env.CLOUDINARY_API_KEY,
+  api_secret: env.CLOUDINARY_API_SECRET,
   secure: true,
 });
 
@@ -13,9 +14,7 @@ export default async function handler(
   res: NextApiResponse,
 ) {
   const results = await cloudinary.search
-    .expression(
-      "systemdni*",
-    )
+    .expression("systemdni*")
     .sort_by("created_at", "desc")
     .execute();
 
